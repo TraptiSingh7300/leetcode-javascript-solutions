05e2f20c523fba20687ffc2cacd2138db014f670
Trapti Singh <114866607+TraptiSingh7300@users.noreply.github.com>
2026-08-13 16:20:47 +0530

    Time: 32 ms (37.03%), Space: 54.4 MB (74.85%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j];
+            }
+        }
+    }
+    return "Not found";
+}
+
+const nums = [2,7,11,15];
+
+console.log(twoSum(nums,9));
