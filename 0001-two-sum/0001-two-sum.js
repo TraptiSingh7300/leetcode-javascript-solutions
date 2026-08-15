cd926270e0e19ad60683ec1ce94bee6b27f40fae
Trapti Singh <114866607+TraptiSingh7300@users.noreply.github.com>
2026-08-15 12:53:34 +0530

    Time: 0 ms (100%), Space: 53.6 MB (93.78%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,19 +1,44 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-var twoSum = function(nums, target) {
-    for(let i=0; i<nums.length; i++){
-        for(let j=i+1; j<nums.length; j++){
-            if(nums[i]+nums[j]===target){
-                return [i,j];
-            }
-        }
+* @param {number[]} nums
+* @param {number} target
+* @return {number[]}
+*/
+
+// Approch - 1
+
+// var twoSum = function(nums, target) {
+//     for(let i=0; i<nums.length; i++){
+//         for(let j=i+1; j<nums.length; j++){
+//             if(nums[i]+nums[j]===target){
+//                 return [i,j];
+//             }
+//         }
+//     }
+//     return "Not found";
+// }
+
+// const nums = [2,7,11,15];
+
+// console.log(twoSum(nums,9));
+
+// Approch - 2
+
+var twoSum = function(nums,target){
+  const obj = {};
+
+  for(let i=0; i<nums.length; i++){
+
+    key = target - nums[i];
+
+    if(key in obj){
+      return [obj[key],i];
     }
-    return "Not found";
+
+    obj[nums[i]] = i;
+  }
+  return "Not found";
 }
 
-const nums = [2,7,11,15];
+const arr = [2,5,7,11,15];
 
-console.log(twoSum(nums,9));
+console.log(twoSum(arr,9));
