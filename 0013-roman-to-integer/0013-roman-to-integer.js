49bbc93a1b48eddc90206ef6f5ee8b82ed0d399b
Trapti Singh <114866607+TraptiSingh7300@users.noreply.github.com>
2026-08-16 23:03:04 +0530

    Time: 5 ms (55.54%), Space: 61 MB (58.76%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const obj = { I:1, V:5, X:10, L:50, C:100, D:500, M:1000 };
+    let number = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (obj[s[i]] < obj[s[i + 1]]) {
+            number -= obj[s[i]];
+        } else {
+            number += obj[s[i]];
+        }
+    }
+
+    return number;
+};
+
+console.log(romanToInt("MCMXCIV"));
